53e001d94b332785f1b1395381d2e88882cbef24
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-09 22:53:38 +0200

    add links

--- src/Routes.js
+++ src/Routes.js
@@ -2,16 +2,18 @@ import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Header, Loading } from "./components";
-import { Home } from "./pages";
-const AboutLazy = lazy(() => import(`./pages/About/About`))
+import { Home, Register, Login } from "./pages";
+const AboutLazy = lazy(() => import(`./pages/About/About`));
 
 function Routes() {
   return (
     <Router>
       <Header />
-      <Suspense fallback={<Loading/>}>
+      <Suspense fallback={<Loading />}>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
           <Route exact path="/about" component={AboutLazy} />
         </Switch>
       </Suspense>
