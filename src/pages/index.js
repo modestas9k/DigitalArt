2c6443c928c3590e50a4e039b57de42faa4300d9
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-09 22:52:34 +0200

    export register and login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
 export { default as Home } from "./Home/Home";
 export { default as About } from "./About/About";
+export { default as Register } from "./Register/Register";
+export { default as Login } from "./Login/Login";
