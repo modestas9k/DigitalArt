8f4f3cd760e63940bb4a86f2d78f6a9358c430a2
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-09 22:54:24 +0200

    add firebase auth to navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
 import "./Header.scss";
-import { Link } from "react-router-dom";
-import logoImg from "../../assets/logo.png";
 
 function Header() {
+  const [user, setUser] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(true);
+      } else {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <div className="header">
-      <Link to="/">
-        <img className="img-logo" src={logoImg} alt="Logo" />
+      <Link className="logo" to="/">
+        <h1>Digital_Art</h1>
       </Link>
       <nav>
-        <Link className="styled-link" to="/">
-          Home
-        </Link>
-        <Link className="styled-link" to="/about">
-          About
-        </Link>
+        {user && (
+          <>
+            <Link className="styled-link" to="/">
+              Home
+            </Link>
+            <Link className="styled-link" to="/about">
+              About
+            </Link>
+            <button
+              onClick={() => {
+                firebase.auth().signOut();
+                history.push("/");
+              }}
+            >
+              Logout
+            </button>
+          </>
+        )}
+        {!user && (
+          <>
+            <Link to="/register" className="styled-link">
+              Register
+            </Link>
+            <Link to="/login" className="styled-link">
+              Login
+            </Link>
+          </>
+        )}
       </nav>
     </div>
   );
