f044047ad616cbf847db08a89321112fe85afefa
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-10 22:00:11 +0200

    auth context with firebase

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,9 +1,18 @@
 import React, { useState, createContext } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [state, setState] = useState();
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setState(user.uid);
+    } else {
+      setState("");
+    }
+  });
   return (
     <AuthContext.Provider value={{ state, setState }}>
       {children}
