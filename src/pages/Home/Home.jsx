6aee629929cb8dab9a7e1ab85796b0f39849e8ee
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-11 22:05:11 +0200

    add posts to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,40 @@
-import React from "react";
-import { Section } from "../../components";
+import React, { useState, useEffect } from "react";
+import { Section, Post } from "../../components";
+import firebase from "firebase/app";
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            post: doc.data(),
+          }))
+        );
+      });
+  }, []);
+
   return (
     <>
       <Section>
         <h1>Home</h1>
+        {posts &&
+          posts.map(({ id, post }) => {
+            return (
+              <Post
+                key={id}
+                username={post.username}
+                caption={post.caption}
+                imageURL={post.imageURL}
+              />
+            );
+          })}
       </Section>
     </>
   );
