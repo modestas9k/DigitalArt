539b7b84de62ad59e2b16de2b7f3bf36be70fe00
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-14 22:15:27 +0200

    add welcome section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,30 @@ import { Section, Post } from "../../components";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "./Home.scss";
+import {
+  TextField,
+  Typography,
+  Container,
+  Grid,
+  makeStyles,
+} from "@material-ui/core";
 
-function Home() {
+const useStyles = makeStyles({
+  homeWelcome: {
+    marginTop: "-21px",
+    height: (props) => (props.small ? "20vh" : "60vh"),
+    color: "rgb(235, 235, 235)",
+    marginBottom: "16px",
+  },
+  TextField: {
+    background: "rgb(235, 235, 235)",
+  },
+});
+
+function Home(props) {
   const [posts, setPosts] = useState([]);
+  const classes = useStyles(props);
 
   useEffect(() => {
     firebase
@@ -21,22 +42,45 @@ function Home() {
         );
       });
   }, []);
+
   return (
     <>
+      <Section className="home__welcome">
+        <Container className={classes.homeWelcome}>
+          <Typography component="div" style={{ height: "10vh" }} />
+          <Typography variant="h1">Digital Art</Typography>
+          <Typography variant="subtitle1" color="inherit">
+            Place where artists can share and sell there work. Powered by
+            creators.
+          </Typography>
+
+          <TextField
+            className={classes.TextField}
+            label="Search..."
+            color="primary"
+            margin="normal"
+            variant="filled"
+            fullWidth={true}
+          />
+        </Container>
+      </Section>
       <Section>
-        <h1>Home</h1>
-        {posts &&
-          posts.map(({ id, post }) => {
-            return (
-              <Post
-                key={id}
-                userImage={post.userImage}
-                username={post.username}
-                caption={post.caption}
-                imageURL={post.imageURL}
-              />
-            );
-          })}
+        <Grid container spacing={2}>
+          {posts &&
+            posts.map(({ id, post }) => {
+              return (
+                <Grid key={id} xs={12} sm={6} md={4} item>
+                  <Post
+                    key={id}
+                    userImage={post.userImage}
+                    username={post.username}
+                    caption={post.caption}
+                    imageURL={post.imageURL}
+                  />
+                </Grid>
+              );
+            })}
+        </Grid>
       </Section>
     </>
   );
