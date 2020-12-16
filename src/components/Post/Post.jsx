bb04a9736bc22d42c3dbcef5809a8ccc11e9aa11
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-16 21:39:20 +0200

    change style

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,19 +1,60 @@
 import React from "react";
-import "./Post.scss";
-import Avatar from "@material-ui/core/Avatar";
+import {
+  Paper,
+  Button,
+  Typography,
+  makeStyles,
+  Box,
+  Container,
+  IconButton,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import SettingsIcon from "@material-ui/icons/Settings";
 
-function Post({ imageURL, username, caption, userImage }) {
+const useStyles = makeStyles({
+  image: {
+    width: "100%",
+    objectFit: "contain",
+    borderRadius: 3,
+  },
+  myProfile: {
+    display: "flex",
+    alignItems: "center",
+    //justifyItems: "space-around",
+  },
+});
+
+function Post({ imageURL, username, caption, userImage, userId }) {
+  const history = useHistory();
+  const classes = useStyles();
+
   return (
-    <div className="post">
-      <div className="post__header">
-        <Avatar className="post__avatar" alt={username} src={userImage} />
-        <h3>{username}</h3>
+    <Paper>
+      <Box width="100%">
+        <img className={classes.image} src={imageURL} alt={caption} />
+      </Box>
+      <div style={{ display: "flex" }}>
+        {userId && userId === "#" && (
+          <Container>
+            <Typography variant="subtitle2">{caption}</Typography>
+          </Container>
+        )}
+        {userId.length > 1 && (
+          <>
+            <Button
+              variant="text"
+              color="primary"
+              onClick={() => history.push(`/profile/${userId}`)}
+            >
+              {username}
+            </Button>
+            <Typography variant="subtitle2" style={{ alignSelf: "center" }}>
+              {caption}
+            </Typography>
+          </>
+        )}
       </div>
-      <img className="post__image" src={imageURL} alt="winter" />
-      <h4 className="post__text">
-        <strong>{username}</strong> {caption}
-      </h4>
-    </div>
+    </Paper>
   );
 }
 
