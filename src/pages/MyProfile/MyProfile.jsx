f504ec103ba65e0bc3063d304b1beea9e4e3e4c6
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-18 21:13:38 +0200

    add modal for delete a post

--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Section } from "../../components";
+import { Post } from "../../components";
 import { useHistory } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import firebase from "firebase/app";
@@ -18,7 +18,6 @@ import {
   Button,
 } from "@material-ui/core";
 import SettingsIcon from "@material-ui/icons/Settings";
-import DeleteIcon from "@material-ui/icons/Delete";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,6 +39,8 @@ const useStyles = makeStyles((theme) => ({
     width: theme.spacing(17),
     height: theme.spacing(17),
     marginRight: 20,
+    marginBottom: 20,
+    marginInline: "auto",
   },
   modal: {
     display: "flex",
@@ -56,9 +57,10 @@ const useStyles = makeStyles((theme) => ({
 function MyProfile() {
   const classes = useStyles();
   const auth = useContext(AuthContext);
-  const [userPosts, setUserPosts] = useState();
   const history = useHistory();
-  const [modalState, setModalState] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [deletedPostId, setDeletedPostId] = useState();
+  const [userPosts, setUserPosts] = useState();
   const [userData, setUserData] = useState({
     name: "",
     bio: "",
@@ -73,28 +75,32 @@ function MyProfile() {
       if (user && mounted) {
         getUserPosts(user);
 
-        firebase
-          .firestore()
-          .collection("users")
-          .doc(user.uid)
-          .get()
-          .then((doc) => {
-            if (doc.data()) {
-              setUserData({
-                name: doc.data().name,
-                bio: doc.data().bio,
-                profileImage: doc.data().profileImage,
-                imageRef: doc.data().imageRef,
-              });
-            } else {
-              console.log("not found");
-            }
-          });
+        getUserInfo(user);
       }
     });
 
     return () => (mounted = false);
   }, [auth]);
+
+  function getUserInfo(user) {
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(user.uid)
+      .get()
+      .then((doc) => {
+        if (doc.data()) {
+          setUserData({
+            name: doc.data().name,
+            bio: doc.data().bio,
+            profileImage: doc.data().profileImage,
+            imageRef: doc.data().imageRef,
+          });
+        } else {
+          console.log("not found");
+        }
+      });
+  }
 
   function getUserPosts(user) {
     firebase
@@ -111,8 +117,8 @@ function MyProfile() {
       });
   }
 
-  function deletePost(postId, imageRef) {
-    const imageRefPath = firebase.storage().ref().child(imageRef);
+  function deletePost(post) {
+    const imageRefPath = firebase.storage().ref().child(post.ref);
     imageRefPath
       .delete()
       .then(() => {
@@ -120,7 +126,7 @@ function MyProfile() {
       })
       .catch((error) => console.log(error));
 
-    const postRef = firebase.firestore().collection("posts").doc(postId);
+    const postRef = firebase.firestore().collection("posts").doc(post.id);
     postRef
       .delete()
       .then(() => console.log("success in firestore deletion"))
@@ -128,98 +134,126 @@ function MyProfile() {
   }
 
   return (
-    <Section>
-      <Container className={classes.profileWrapper}>
-        <div>
-          <Avatar
-            className={classes.avatarWrapper}
-            src={userData.profileImage}
-          />
-        </div>
-        <Container>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "flex-start",
-            }}
-          >
-            <Typography variant="h4">{userData.name}</Typography>
-            <IconButton onClick={() => history.push("/editProfile")}>
-              <SettingsIcon />
-            </IconButton>
-          </div>
-          <Typography variant="subtitle2">{userData.bio} </Typography>
-        </Container>
+    <>
+      <Container>
+        <Modal
+          className={classes.modal}
+          open={deleteModal}
+          onClose={() => setDeleteModal(false)}
+        >
+          <Paper className={classes.paper}>
+            <Typography variant="h5">Delete this Post? </Typography>
+            <div className={classes.root}>
+              <Button
+                onClick={() => {
+                  deletePost(deletedPostId);
+                  console.log(deletedPostId);
+                  setDeleteModal(false);
+                }}
+                variant="contained"
+                color="secondary"
+              >
+                Yes
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => setDeleteModal(false)}
+              >
+                No
+              </Button>
+            </div>
+          </Paper>
+        </Modal>
+        <Grid container className={classes.profileWrapper}>
+          <Grid item xs={5} sm={4}>
+            <Avatar
+              className={classes.avatarWrapper}
+              src={userData.profileImage}
+            />
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            {userData.name !== "" && (
+              <>
+                <Box
+                  mx="auto"
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "flex-start",
+                  }}
+                >
+                  <Typography variant="h4">{userData.name}</Typography>
+                  <IconButton onClick={() => history.push("/editProfile")}>
+                    <SettingsIcon />
+                  </IconButton>
+                </Box>
+                <Typography variant="subtitle2">{userData.bio} </Typography>
+              </>
+            )}
+            {userData.name === "" && userData.bio === "" && (
+              <Box>
+                <Typography variant="body1">
+                  You don't have any information about your self.
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => history.push("/editProfile")}
+                >
+                  Get started
+                </Button>
+              </Box>
+            )}
+          </Grid>
+        </Grid>
       </Container>
       <Container>
+        <Box marginBottom={2}>
+          <Button
+            color="primary"
+            variant="outlined"
+            onClick={() => history.push("/upload")}
+          >
+            Upload
+          </Button>
+        </Box>
+
         <Grid container spacing={2}>
+          {userPosts && userPosts.length === 0 && (
+            <Box mx="auto" mt={16}>
+              <Typography variant="h6">No images added</Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => history.push("/upload")}
+              >
+                Upload image
+              </Button>
+            </Box>
+          )}
           {userPosts &&
             userPosts.map(({ id, post }) => {
               return (
                 <Grid key={id} xs={12} sm={6} md={4} item>
-                  <Paper>
-                    <Box width="100%">
-                      <img
-                        className={classes.image}
-                        src={post.imageURL}
-                        alt={post.imageRef}
-                      />
-                    </Box>
-                    <Container
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                      }}
-                    >
-                      <Typography variant="subtitle2">
-                        {post.caption}
-                      </Typography>
-                      <IconButton
-                        variant="text"
-                        color="default"
-                        onClick={(e) => {
-                          setModalState(true);
-                        }}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                      <Modal
-                        className={classes.modal}
-                        open={modalState}
-                        onClose={() => setModalState(false)}
-                      >
-                        <Paper className={classes.paper}>
-                          <Typography variant="h5">
-                            Delete this Post?
-                          </Typography>
-                          <div className={classes.root}>
-                            <Button
-                              onClick={() => deletePost(id, post.imageRef)}
-                              variant="contained"
-                              color="secondary"
-                            >
-                              Yes
-                            </Button>
-                            <Button
-                              variant="contained"
-                              color="primary"
-                              onClick={() => setModalState(false)}
-                            >
-                              No
-                            </Button>
-                          </div>
-                        </Paper>
-                      </Modal>
-                    </Container>
-                  </Paper>
+                  <Post
+                    key={id}
+                    userId="myProfile"
+                    userImage={post.userImage}
+                    username={post.username}
+                    caption={post.caption}
+                    imageURL={post.imageURL}
+                    handleDeleteButton={(e) => {
+                      setDeleteModal(true);
+                      setDeletedPostId({ id: id, ref: post.imageRef });
+                    }}
+                  />
                 </Grid>
               );
             })}
         </Grid>
       </Container>
-    </Section>
+    </>
   );
 }
 
