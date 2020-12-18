ebe10bb38cae439e4c13f8156e746f1cce8edc14
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-18 21:15:09 +0200

    small stuff

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -8,24 +8,17 @@ import {
   Container,
   makeStyles,
   Grid,
+  Box,
 } from "@material-ui/core";
-import { Section, Post } from "../../components";
+import { Post } from "../../components";
 import { useHistory, useParams } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-  },
   profileWrapper: {
     display: "flex",
-    marginTop: 20,
-    marginBottom: 20,
+    justifyContent: "center",
+    marginTop: theme.spacing(6),
+    marginBottom: theme.spacing(8),
   },
   avatarWrapper: {
     width: theme.spacing(17),
@@ -76,54 +69,49 @@ export default function Profile(props) {
 
   return (
     <>
-      <Section>
-        {profileData && (
-          <>
-            <Container className={classes.profileWrapper}>
-              <div>
-                <Avatar
-                  className={classes.avatarWrapper}
-                  src={profileData.profileImage}
-                />
-              </div>
-              <div>
-                <Typography variant="h4">{profileData.name}</Typography>
-                <Typography variant="subtitle2">{profileData.bio} </Typography>
-              </div>
-            </Container>
-            <Container>
-              <Grid container spacing={2}>
-                {userPosts &&
-                  userPosts.map(({ id, post }) => {
-                    return (
-                      <Grid key={id} xs={12} sm={6} md={4} item>
-                        <Post
-                          key={id}
-                          userId="#"
-                          userImage={post.userImage}
-                          username={post.username}
-                          caption={post.caption}
-                          imageURL={post.imageURL}
-                        />
-                      </Grid>
-                    );
-                  })}
-              </Grid>
-            </Container>
-          </>
-        )}
-        {!profileData && (
-          <Container>
-            <Typography variant="h2">Sorry, no user found</Typography>
-            <Button
-              onClick={() => history.push("/")}
-              className={classes.button}
-            >
-              Home
-            </Button>
+      {profileData && (
+        <>
+          <Container className={classes.profileWrapper}>
+            <Box>
+              <Avatar
+                className={classes.avatarWrapper}
+                src={profileData.profileImage}
+              />
+            </Box>
+            <Box>
+              <Typography variant="h4">{profileData.name}</Typography>
+              <Typography variant="subtitle2">{profileData.bio} </Typography>
+            </Box>
           </Container>
-        )}
-      </Section>
+          <Container>
+            <Grid container spacing={2}>
+              {userPosts &&
+                userPosts.map(({ id, post }) => {
+                  return (
+                    <Grid key={id} xs={12} sm={6} md={4} item>
+                      <Post
+                        key={id}
+                        userId="profile"
+                        userImage={post.userImage}
+                        username={post.username}
+                        caption={post.caption}
+                        imageURL={post.imageURL}
+                      />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </>
+      )}
+      {!profileData && (
+        <Container>
+          <Typography variant="h2">Sorry, no user found</Typography>
+          <Button onClick={() => history.push("/")} className={classes.button}>
+            Home
+          </Button>
+        </Container>
+      )}
     </>
   );
 }
