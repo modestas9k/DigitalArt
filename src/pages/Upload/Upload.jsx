064fd9a6cf4ae35cf86ae14612b702c8b7ab669f
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-18 21:22:33 +0200

    changed all to to materialUi + error handle

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,28 +1,79 @@
 import React, { useState } from "react";
-import { Section, Input, Button } from "../../components";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage";
 import "firebase/auth";
-import "./Upload.scss";
+import {
+  Paper,
+  Container,
+  Typography,
+  Box,
+  Button,
+  Input,
+  Checkbox,
+  FormControlLabel,
+  makeStyles,
+  LinearProgress,
+  FormControl,
+  FormHelperText,
+} from "@material-ui/core";
+import ChipInput from "material-ui-chip-input";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "90vh",
+  },
+  spacing: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  input: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
 
 function Upload() {
+  const history = useHistory();
+  const classes = useStyles();
   const [image, setImage] = useState();
-  const [fileError, setFileError] = useState();
-  const [captionError, setCaptionError] = useState();
+  const [caption, setCaption] = useState([]);
+  const [checked, setChecked] = useState(false);
   const [progress, setProgress] = useState();
-  const [caption, setCaption] = useState();
-  const [message, setMessage] = useState();
+  const [inProgress, setInProgress] = useState(false);
+  const [captionError, setCaptionError] = useState();
+  const [imageError, setImageError] = useState();
 
-  const handleChange = (e) => {
+  const handleChecked = (event) => {
+    setChecked(event.target.checked);
+  };
+  const handleChips = (chip) => {
+    setCaption(chip);
+  };
+  const handleImage = (e) => {
     if (e.target.files[0]) {
       setImage(e.target.files[0]);
-      setFileError("");
     }
   };
-  const handleUpload = () => {
-    if (!caption) {
-      return setCaptionError("No caption");
+  function handleUpload() {
+    setInProgress(true);
+    setImageError("");
+    setCaptionError("");
+
+    if (!image) {
+      setImageError("No image selected");
+      setInProgress(false);
+      return;
+    }
+    if (caption.length === 0) {
+      setInProgress(false);
+      setCaptionError("No caption");
+      return;
     }
     if (image) {
       const fileRef = firebase.storage().ref("images").child(image.name);
@@ -33,81 +84,103 @@ function Upload() {
         "state_changed",
         (snapshot) => {
           // progress function
-          const progress = Math.round(
+          const Progress = Math.round(
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100
           );
-          setProgress(progress);
+          setProgress(Progress);
         },
         (error) => {
           // error function
           console.log(error);
-          setCaptionError(error.message);
+          setInProgress(false);
         },
         () => {
           // complete function
-
-          fileRef.getDownloadURL().then((url) => {
-            // post image inside db
-            firebase.firestore().collection("posts").add({
-              timestamp: new Date(),
-              username: firebase.auth().currentUser.displayName,
-              userId: firebase.auth().currentUser.uid,
-              userImage: firebase.auth().currentUser.photoURL,
-              caption: caption,
-              imageURL: url,
-              imageRef: fileRef.fullPath,
+          fileRef
+            .getDownloadURL()
+            .then((url) => {
+              // post image inside firestore
+              firebase.firestore().collection("posts").add({
+                timestamp: new Date(),
+                username: firebase.auth().currentUser.displayName,
+                userId: firebase.auth().currentUser.uid,
+                userImage: firebase.auth().currentUser.photoURL,
+                caption: caption,
+                freeDownload: checked,
+                imageURL: url,
+                imageRef: fileRef.fullPath,
+              });
+            })
+            .then(() => {
+              setInProgress(false);
+              history.push("/myProfile");
             });
-            setCaption("");
-            setProgress("");
-            setImage("");
-            setMessage("Uploaded successfully!");
-          });
         }
       );
     } else {
-      setFileError("No file selected");
+      setImageError("No file selected");
+      setInProgress(false);
     }
-  };
+  }
 
   return (
-    <Section>
-      <h1>Upload Image</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        {message && (
-          <div className="upload__message">
-            <h2>{message}</h2>
-          </div>
-        )}
-        <Input
-          progress={progress}
-          label="profile image"
-          type="file"
-          handleChange={handleChange}
-          required
-        >
-          {fileError && <span>{fileError}</span>}
-        </Input>
-        <Input
-          type="textarea"
-          label="Caption"
-          placeholder="Write caption about your work"
-          handleChange={(e) => {
-            setCaption(e.target.value);
-            setCaptionError("");
-          }}
-          required
-        >
-          {captionError && <span>{captionError}</span>}
-        </Input>
-        <Button handleClick={handleUpload} className="button button-primary">
-          Upload
-        </Button>
-      </form>
-    </Section>
+    <Container className={classes.root} disableGutters>
+      <Paper>
+        <Box p={2}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <Typography gutterBottom variant="h4">
+              Upload image
+            </Typography>
+            <FormControl className={classes.input} error={Boolean(imageError)}>
+              <Input
+                type="file"
+                required
+                className={classes.input}
+                onChange={handleImage}
+              />
+              <FormHelperText>{imageError}</FormHelperText>
+            </FormControl>
+            <ChipInput
+              error={Boolean(captionError)}
+              helperText={captionError}
+              label="Hit Enter to separate captions"
+              variant="standard"
+              className={classes.input}
+              onChange={(chips) => handleChips(chips)}
+            />
+
+            <FormControlLabel
+              className={classes.spacing}
+              control={
+                <Checkbox
+                  checked={checked}
+                  inputProps={{ "aria-label": "secondary checked" }}
+                  onChange={handleChecked}
+                />
+              }
+              label="Allow to download"
+            />
+            {progress && (
+              <LinearProgress variant="determinate" value={progress} />
+            )}
+            <Button
+              className={classes.spacing}
+              onClick={() => handleUpload()}
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={Boolean(inProgress)}
+            >
+              upload
+            </Button>
+          </form>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
 
