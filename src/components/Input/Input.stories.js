c59e97ac6f9298beb61af0cd1520900d2a5daede
Modestas Skirius <modestas.skirius@gmail.com>
2020-12-21 22:42:52 +0200

    clean up

--- src/components/Input/Input.stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-import Input from "./Input";
-
-storiesOf("Input", module).add("Input", () => (
-  <Input label="Name" placeholder="Enter your name" type="text" />
-));
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
 export { default as Header } from "./Header/Header";
-export { default as Section } from "./Section/Section";
 export { default as Loading } from "./Loading/Loading";
-export { default as Input } from "./Input/Input";
 export { default as Post } from "./Post/Post";
 export { default as Login } from "./Login/Login";
